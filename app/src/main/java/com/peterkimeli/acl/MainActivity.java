6280f1eb66e7394c7bf199f565e40e804d023087
kimpetertanui <kimelipeter526@gmail.com>
2019-07-14 18:19:37 +0300

    Initial commits for ALC phase

---
+++ app/src/main/java/com/peterkimeli/acl/MainActivity.java
@@ -0,0 +1,39 @@
+package com.peterkimeli.acl;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button AboutButton;
+    private Button ProfileButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AboutButton = (Button)findViewById(R.id.about_btn);
+        ProfileButton=(Button)findViewById(R.id.profile_btn);
+
+        AboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginRegisterSalonistIntent =new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(LoginRegisterSalonistIntent);
+            }
+        });
+
+        ProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginRegisterCustomerIntent= new Intent(MainActivity.this,ProfileActivity.class);
+                startActivity(LoginRegisterCustomerIntent);
+
+            }
+        });
+    }
+}
